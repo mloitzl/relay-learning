39abbc3366e73e730b04f84af1c69809bfaa9ae4
Martin Loitzl <martin@loitzl.com>
2026-01-16 21:51:36 +0100

    feat: add babel-plugin-relay dependency and update HomePage component structure

--- apps/client/src/pages/Home.tsx
+++ apps/client/src/pages/Home.tsx
@@ -1,23 +1,24 @@
 import { Suspense } from "react";
 import { graphql, useLazyLoadQuery } from "react-relay";
+import { UserCard } from "../components/UserCard";
 
 const HomeQuery = graphql`
-    query HomeQuery {
-        me {
-        ...UserCard_user
-        }
+  query HomeQuery {
+    me {
+      ...UserCard_user
     }
+  }
 `;
 
-export function HomePage(){
-    const queryRef = useLazyLoadQuery(HomeQuery);
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <Suspense fallback={<div>Loading user data...</div>}>
-                {/* @ts-expect-error Relay types */}
-                <UserCard user={queryRef.me} />
-            </Suspense>
-        </div>
-    );
+export function HomePage() {
+  const queryRef = useLazyLoadQuery(HomeQuery, {});
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Suspense fallback={<div>Loading user data...</div>}>
+        {/* @ts-expect-error Relay types */}
+        <UserCard user={queryRef.me} />
+      </Suspense>
+    </div>
+  );
 }
