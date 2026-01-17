2e968dbc8aa439ccaed2409c94038ac82787beed
Martin Loitzl <martin@loitzl.com>
2026-01-17 17:49:14 +0100

    refactor: update Relay configuration files and simplify babel-plugin usage

--- apps/client/relay.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  src: './src',
-  schema: './schema.graphql',
-  language: 'typescript',
-  eagerEsModules: true,
-  artifactDirectory: './src/__generated__',
-  watchman: false,
-};
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: ["babel-plugin-relay"],
+        plugins: ["relay"],
       },
     }),
   ],
