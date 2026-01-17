777a743120180d9e614e269a40b55a754137e683
Martin Loitzl <martin@loitzl.com>
2026-01-17 09:33:45 +0000

    chore: update dependencies and add new packages
    - Added babel-plugin-relay and graphql to the project.
    - Introduced vite-plugin-terminal for enhanced terminal integration.
    - Updated various packages including rollup and its plugins.
    - Added peer dependencies for rollup in relevant plugins.
    - Included new types for parse-json and added error-ex for better error handling.
    - Updated existing packages to their latest versions for improved stability and performance.

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Terminal from 'vite-plugin-terminal';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({ babel: { plugins: ["relay"] } }),
+    Terminal()
+  ],
   server: {
     port: 5173,
     host: '0.0.0.0',
